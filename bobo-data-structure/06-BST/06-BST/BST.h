be82db20a918095fc1d3139517aaaceeec3baac0
huangwei <harw.op@qq.com>
2022-04-17 19:29:34 +0800

    [add] BST add 'add' method

--- bobo-data-structure/06-BST/06-BST/BST.h
+++ bobo-data-structure/06-BST/06-BST/BST.h
@@ -29,9 +29,35 @@ public:
         root = nullptr;
         size = 0;
     }
+    
+    void add(T e) {
+        if (root == nullptr) {
+            root = new Node<T>(e);
+            return;
+        }
+        
+        add(root, e);
+    }
+    
+    
 private:
     Node<T>* root;
     int size;
+    
+    Node<T>* add(Node<T> *node, T e) {
+        if (node == nullptr) {
+            return new Node<T>(e);
+            size += 1;
+        } else {
+            if (e < node -> val) {
+                node -> left = add(node -> left, e);
+            } else if (e > node -> val) {
+                node -> right = add(node -> right, e);
+            } else {
+                return node;
+            }
+        }
+    }
 };
 
 #endif /* BST_h */
